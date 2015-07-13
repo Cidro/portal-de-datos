b9d9ad89c7aa3c53f0628838e42ff83845e28115
cidro <pablo.canaleshernandez@gmail.com>
2015-07-13 13:49:08 -0400

    Se corrige error con columna de las vistas junar

--- application/models/Entities/VistaJunar.php
+++ application/models/Entities/VistaJunar.php
@@ -55,11 +55,6 @@ class VistaJunar
     private $table_id;
 
     /**
-     * @var string
-     */
-    private $type;
-
-    /**
      * @var \DateTime
      */
     private $created_at;
@@ -326,16 +321,6 @@ class VistaJunar
         $this->recurso = $recurso;
     
         return $this;
-    }
-
-    public function setType($type){
-        $this->type = $type;
-
-        return $this;
-    }
-
-    public function getType(){
-        return $this->type;
     }
 
     /**
